a5ac290b5bc5cff5104bf8e78b2c9ec71f2b9cce
Ara <oladipoeyiara@gmail.com>
2023-05-05 13:39:03 -0400

    Setting up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "minim-gray-a": "#1D1D1D",
+        "minim-gray-b": "#1A1A1A",
+      },
+      backgroundImage: {
+        "hexagon-pattern":
+          "url('./src/assets/backgrounds/hexagon-pattern.png')",
+      },
+    },
   },
   plugins: [],
 };
