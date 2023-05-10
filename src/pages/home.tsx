815ad5724b36b0beae5b48fc9c708e4da408d8ab
Ara <oladipoeyiara@gmail.com>
2023-05-10 14:54:14 -0400

    Adding CK editor to main page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import PencilIcon from "../assets/pencil-icon.png";
 import Sidebar from "../components/home/Sidebar";
-
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import BalloonEditor from "@ckeditor/ckeditor5-build-balloon";
 export default function Home() {
   return (
     <>
@@ -24,8 +26,41 @@ export default function Home() {
               </h3>
             </div>
           </div>
-          <div className="bg-black h-full w-full">
-            <h3>hi</h3>
+          <div className="bg-black h-full w-full notes-area">
+            <CKEditor
+              editor={BalloonEditor}
+              data="<p>Hello from CKEditor 5!</p>"
+              onReady={(editor) => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              config={{
+                toolbar: [
+                  "heading",
+                  "|",
+                  "bold",
+                  "italic",
+                  "blockQuote",
+                  "bulletedList",
+                  "numberedList",
+                  "fontSize",
+                  "link",
+                  "fontFamily",
+                  "undo",
+                  "redo",
+                ],
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
           </div>
         </section>
       </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
+import react from "@vitejs/plugin-react";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ckeditor5({ theme: require.resolve("@ckeditor/ckeditor5-theme-lark") }),
+  ],
+});
